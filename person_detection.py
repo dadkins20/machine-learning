de85031103fa354dec685185acb99a6f8929d82a
David Adkins <david@davidadkins.com>
2019-11-13 21:10:50 -0500

    updated code

--- person_detection.py
+++ person_detection.py
@@ -90,8 +90,8 @@ NUM_CLASSES = 90
 # dictionary mapping integers to appropriate string labels would be fine
 label_map = label_map_util.load_labelmap(PATH_TO_LABELS)
 categories = label_map_util.convert_label_map_to_categories(label_map, max_num_classes=NUM_CLASSES, use_display_name=True)
-list(filter(lambda c: c['id'] == 1, categories))
-print(categories)
+x = list(filter(lambda c: c['id'] in [1], categories))
+print(x)
 exit()
 category_index = label_map_util.create_category_index(categories)
 
